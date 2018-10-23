dc10849205e8bfdce479728c9561ea5746daef56
Crozet Mathieu <matcrid@hotmail.fr>
2018-10-23 13:29:35 +0200

    Update js/diaporama.js

--- js/diaporama.js
+++ js/diaporama.js
@@ -25,6 +25,36 @@ $('#contenu1').css({
     "transition-duration": "3s"
 });
 
+function diapFirst(){
+    $('#contenu1').css(modification[2]);
+    for (var i = nombreContenu; i > 1; i--) {
+        $('#contenu'+i).css(modification[0]); // Boucles car modications identiques
+    }
+};
+
+function diapLast(){
+    $('#contenu' + nombreContenu).css(modification[2]);
+    for (var i = nombreContenu - 1; i > 0; i--) {
+        $('#contenu'+i).css(modification[1]);
+    }
+};
+
+function diapMid(){
+    for (var i = nombreContenu; i > 0; i--) { // boucle pour modifier le css quand la div selectionnée est au milieu
+        var currentI = i;                       
+        if(currentI > chiffreContenu) {
+            $('#contenu'+currentI).css(modification[0]);
+        }
+        else if(currentI == chiffreContenu) {
+            $('#contenu'+currentI).css(modification[2]);
+        }
+        else if(currentI < chiffreContenu) {
+            $('#contenu'+currentI).css(modification[1]);
+        }               
+    }
+};
+
+
 /*
 --------- Animation Manuelle Click ----------
 */
@@ -36,31 +66,14 @@ $('div').on('click', function () {
     var id = this.id;
 
     if(id === "contenu1"){ // Si l'id de la div selectionné est contenu 1 alors on applique une modification CSS sur elle meme puis une boucle pour les autres 
-        $('#contenu1').css(modification[2]);
-        for (var i = nombreContenu; i > 1; i--) {
-            $('#contenu'+i).css(modification[0]); // Boucles car modications identiques
-        }
+        diapFirst();        
     } 
     else if(id === "contenu" + nombreContenu){ // idem que précédent sauf si c'est la dernière div sélectionnée
-        $('#contenu' + nombreContenu).css(modification[2]);
-        for (var i = nombreContenu - 1; i > 0; i--) {
-            $('#contenu'+i).css(modification[1]);
-        }
+        diapLast();
     }   
     else if((id !== "contenu" + nombreContenu) && (id !== "contenu1")){ 
-        var chiffreContenu = id.substr(7);
-        for (var i = nombreContenu; i > 0; i--) { // boucle pour modifier le css quand la div selectionnée est au milieu
-            var currentI = i;                       
-            if(currentI > chiffreContenu) {
-                $('#contenu'+currentI).css(modification[0]);
-            }
-            else if(currentI == chiffreContenu) {
-                $('#contenu'+currentI).css(modification[2]);
-            }
-            else if(currentI < chiffreContenu) {
-                $('#contenu'+currentI).css(modification[1]);
-            }               
-        }
+        window['chiffreContenu'] = id.substr(7);
+        diapMid();
     }
 });
 
@@ -72,51 +85,23 @@ function infosClavier(e) {
     if(e.keyCode === 39){
         if (diap === nombreContenu) {
             diap = 1;
-            $('#contenu1').css(modification[2]);
-            for (var i = nombreContenu; i > 1; i--) {
-                $('#contenu'+i).css(modification[0]);
-            }
+            diapFirst();
         }
         else{
             diap++;
-            var chiffreContenu = diap;
-            for (var i = nombreContenu; i > 0; i--) { // boucle pour modifier le css quand la div selectionnée est au milieu
-                var currentI = i;                       
-                if(currentI > chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[0]);
-                }
-                else if(currentI == chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[2]);
-                }
-                else if(currentI < chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[1]);
-                }               
-            }
+            window['chiffreContenu'] = diap;
+            diapMid();
         }
     }
     else if(e.keyCode === 37){
         if (diap === 1) {
             diap = nombreContenu;
-            $('#contenu' + nombreContenu).css(modification[2]);
-            for (var i = nombreContenu - 1; i > 0; i--) {
-                $('#contenu'+i).css(modification[1]);
-            }
+            diapLast();
         }
         else{
             diap--;
-            var chiffreContenu = diap;
-            for (var i = nombreContenu; i > 0; i--) { // boucle pour modifier le css quand la div selectionnée est au milieu
-                var currentI = i;                       
-                if(currentI > chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[0]);
-                }
-                else if(currentI == chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[2]);
-                }
-                else if(currentI < chiffreContenu) {
-                    $('#contenu'+currentI).css(modification[1]);
-                }               
-            }
+            window['chiffreContenu'] = diap;
+            diapMid();
         }
     }
 }
@@ -134,30 +119,11 @@ var diap3Auto = false;
 var diap4Auto = false;
 
 function animationAuto(){
-    if(diap1Auto){
-            diap2();
-            diap1Auto = false;
-            diap2Auto = true;      
-    }
-    else if(diap2Auto){
-            diap3();
-            diap2Auto = false;
-            diap3Auto = true;      
-    }
-    else if(diap3Auto){
-            diap4();
-            diap3Auto = false;
-            diap4Auto = true;      
-    }
-    else if(diap4Auto){
-            diap1();
-            diap4Auto = false;
-            diap1Auto = true;      
-    }
+    
 };
 
 function demarrer(){
-    setInterval(animationAuto(), 50);
+    setInterval(animationAuto(), 5000);
 };
 
 
